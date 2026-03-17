c6a152ff2d7dfa8755a4eaaf89936a1797aae66c
mahaandika <agungandikabali@gmail.com>
2026-03-17 21:29:43 +0800

    fix: add fillable and relation to project_images model

--- app/Models/ProjectImage.php
+++ app/Models/ProjectImage.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProjectImage extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * Atribut yang dapat diisi secara massal.
+     */
+    protected $fillable = [
+        'project_id',
+        'image_path',
+        'order',
+    ];
+
+    /**
+     * Relasi: Gambar ini dimiliki oleh satu Project.
+     * * @return BelongsTo
+     */
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
