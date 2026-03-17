07d799d17c87a685956e4f82c2fb1dddc127d1f6
mahaandika <agungandikabali@gmail.com>
2026-03-17 21:26:56 +0800

    fix: add fillable and relation to category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * Atribut yang dapat diisi (mass assignable).
+     */
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    /**
+     * Relasi: Satu Kategori memiliki banyak Project.
+     * * @return HasMany
+     */
+    public function projects(): HasMany
+    {
+        // Category hasMany Project
+        return $this->hasMany(Project::class);
+    }
 }
