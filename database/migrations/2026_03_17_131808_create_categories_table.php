abc11453fcda32c019c5f87796effe9c2f63acd3
mahaandika <agungandikabali@gmail.com>
2026-03-18 10:56:44 +0800

    fix: add status to migration

--- database/migrations/2026_03_17_131808_create_categories_table.php
+++ database/migrations/2026_03_17_131808_create_categories_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
+            $table->string('status')->default('active');
             $table->timestamps();
         });
     }
