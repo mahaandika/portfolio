7963755073056267be14587ee9a04390658a88ce
mahaandika <agungandikabali@gmail.com>
2026-03-18 11:19:51 +0800

    fix: add index function

--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Experience;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ExperienceController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('admin/experiences/index', [
+            'experiences' => Experience::latest()->get()
+        ]);
+    }
 }
