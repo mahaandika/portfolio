a659fb607fcb1f63a0ba20329d45bae4f8695726
mahaandika <agungandikabali@gmail.com>
2026-03-18 21:48:58 +0800

    fix: call home component

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -12,7 +12,7 @@ export default function Welcome({ categories, experiences, projects }: Props) {
     return (
         <div>
             <Hero />
-            <div className="h-96"></div>
+            {/* <div className="h-96"></div> */}
         </div>
     );
 }
