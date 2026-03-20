13e8d0450189e8182100c2d20abdfea6ca1586a3
mahaandika <agungandikabali@gmail.com>
2026-03-20 11:31:47 +0800

    fix: call projects component

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,6 +1,7 @@
 import About from '@/components/about';
 import Expertise from '@/components/expertise';
 import Hero from '@/components/hero';
+import Projects from '@/components/projects';
 import { Head, Link } from '@inertiajs/react';
 import { Briefcase, Code2, Folder } from 'lucide-react';
 
@@ -16,6 +17,7 @@ export default function Welcome({ categories, experiences, projects }: Props) {
             <Hero />
             <About />
             <Expertise experiences={experiences} />
+            <Projects projects={projects} />
         </div>
     );
 }
